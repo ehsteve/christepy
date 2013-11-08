19853e629e8e8112a55b38790c517635305b30ba
Steven Christe <steven.d.christe@nasa.gov>
2013-11-08 15:00:36 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='christepy',
+      version='1.0',
+      description='Steven Christe's personal Python Utilities',
+      author='Steven Christe',
+      author_email='steven.christe@nasa.gov',
+      url='https://github.com/ehsteve/christepy',
+      packages=['christepy'],
+     )
